1540cbe2476300e187a90626deb0e18cf1a8c656
petrero <petrero@gmail.com>
2022-05-27 11:21:53 +0300

    4.5. Wildcard Routes
      But I want more fancy! Add another line with $title = then type u and auto-complete a function that's literally called... u.
    
      We don't use many functions from Symfony, but this is a rare example. This comes from a Symfony library called symfony/string. As I mentioned, Symfony is many different libraries - also called components - and we're going to leverage those libraries all the time. This one helps you make string transformations... and it happens to already be installed.
    
      Move the str_replace() to the first argument of u(). This function returns an object that we can then do string operations on. One method is called title(). Say ->title(true) to convert all words to title case.
    
      Now whe n we try it... sweet! It uppercases the letters! The string component isn't particularly important, I just want you to see how we can already leverage parts of Symfony to get our job done.

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 class VinylController {
 	#[Route('/')]
@@ -13,7 +14,7 @@ class VinylController {
 
 	#[Route('/browse/{slug}')]
 	public function browse(string $slug): Response {
-		$title = str_replace('-', ' ', $slug);
+		$title = u(str_replace('-', ' ', $slug))->title();
 
 		return new Response('Genre: ' . $title);
 	}
