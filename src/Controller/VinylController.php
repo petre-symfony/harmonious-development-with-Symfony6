e259fb16b12d188a7442dfd0022d6efe146df019
petrero <petrero@gmail.com>
2022-05-29 08:54:28 +0300

    9.3. Profiler: Your Debugging Best Friend
      And this is a lot more powerful - and prettier - than using var_dump(): we can expand sections and see deep data really easily.
    
      Instead of dd(), you can also use dump().. to dump and live. But this might not show up where you expect it to. Instead of printing in the middle of the page, it shows up down in the web debug toolbar, under the target icon.
    
      If that's a bit too small, click to see a bigger version in the profiler.

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -19,8 +19,8 @@ class VinylController extends AbstractController {
 			['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
 		];
 
-		dd($tracks);
-		
+		dump($tracks);
+
 		return $this->render('vinyl/homepage.html.twig', [
 			'title' => 'PB & Jams',
 			'tracks' => $tracks
