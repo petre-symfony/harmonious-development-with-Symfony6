91342a3487d49710b2cc59ba3dcf91adca50b7ee
petrero <petrero@gmail.com>
2022-05-29 15:26:10 +0300

    15.1. The Twig Service & Profiler for API Requests
      Since this page just loaded without an error, we think that we just successfully logged a message via the logger service. But... where do log messages go? How can we check?
    
      The logger service is provided by a library that we installed earlier called monolog. It was part of the debug-pack. And you can control its configuration inside the config/packages/monolog.yaml file, including where log messages are logged to, like which file. We'll focus more on config in the next tutorial.
    
      The Profiler for API Requests
      - But one way that you can always see the log messages for a request is via the profiler! This is super handy. Go to the homepage, click any link on the web debug toolbar... and then go to the Logs section. We're now seeing all the log messages that were made only during that last request to the homepage.
    
      Great! Except that... our log message is made on an API endpoint... and API endpoints don't have a web debug toolbar we can click! Are we stuck? Nope! Refresh this page one more time... then manually go to /_profiler. This is... kind of a secret door into the profiler system... and this page shows the last ten requests made into our system. The second to the top is the API request we just made. Click the little token link to see... yea! We're looking at the profiler for that API request! Over in the Logs section... there it is!
    
        Returning API response for song 5
    
      ... and you can even see the extra info we passed.
    
      Rendering a Twig Template Manually
      - Ok, services are so important that... I want to do one more quick example. Go back to VinylController. The render() method is really just a shortcut to fetch the "twig" service, call some method on that object to render the template... and then put the final HTML string into a Response object. It's a great shortcut and you should use it.
    
      But! As a challenge, could we render a template without using that method? Of course! Let's do it.
    
      Step one: find the service that does the work you need to do. So, we need to find the Twig service. Let's do our trick again:
    
        php bin/console debug:autowiring twig
      And... yes! Apparently the type-hint we need to use is Twig\Environment.
    
      Ok! Go back to our method, add an argument, type Environment, and hit tab to auto-complete that so PhpStorm adds the use statement. Let's call it $twig.
    
      Below, instead of using render, let's say $html = and then $twig->. Like with the logger, we don't need to know what methods this class has, because, thanks to the type-hint, PhpStorm can tell us all the methods. That render() method looks like it's probably what we want. The first argument is the string name of the template to render and the $context argument holds the variables. So... it has the same arguments that we were already passing.
    
      To see if this is working, dd($html).

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -5,11 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController {
 	#[Route('/', name: 'app_homepage')]
-	public function homepage(): Response {
+	public function homepage(Environment $twig): Response {
 		$tracks = [
 			['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
 			['song' => 'Waterfalls', 'artist' => 'TLC'],
@@ -19,10 +20,12 @@ class VinylController extends AbstractController {
 			['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
 		];
 
-		return $this->render('vinyl/homepage.html.twig', [
+		$html = $twig->render('vinyl/homepage.html.twig', [
 			'title' => 'PB & Jams',
 			'tracks' => $tracks
 		]);
+
+		dd($html);
 	}
 
 	#[Route('/browse/{slug}', name: 'app_browse')]
